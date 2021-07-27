e90dcfd3223df08ad63f9f177f5d9c031f827745
Supriya Ojha <supriyaojha28@gmail.com>
2021-07-27 13:37:05 +0530

    version 1.2

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -2,10 +2,9 @@ import {makeStyles,Box, Typography} from '@material-ui/core';
 
 const useStyles=makeStyles({
     image :{
-        background: `url(${'https://png.pngtree.com/background/20210709/original/pngtree-food-menu-fare-meal-background-picture-image_869492.jpg'})`,
-        width: '100%',
-        heigth: '100%'
-        //center/55% repeat-x #000`
+        background:`url("https://png.pngtree.com/background/20210709/original/pngtree-food-menu-fare-meal-background-picture-image_869492.jpg") center/55% repeat-x #000`,
+        width:'100%',
+        height:'40vh'
     }
 })
 
@@ -14,8 +13,9 @@ const Banner=()=>{
     const classes = useStyles();
     return(
         <Box className={classes.image}>
-            
+            <Typography>Hello</Typography>
         </Box>
+
     )
 }
 
